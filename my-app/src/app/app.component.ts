da5a56f5a447ecbb0ee5796657b2f3788b8a7c25
Quentin <quentin.cuesta@efrei.net>
2017-10-03 10:22:25 +0200

    Merge branch 'feature/5' into develop

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+  collection: any;
+
+  ngOnInit(): void {
+    this.collection = [
+      {reference: '1111', name: 'Marina'},
+      {reference: '2222', name: 'Patrick'},
+      {reference: '3333', name: 'Quentin'}
+    ];
+  }
+
 }
